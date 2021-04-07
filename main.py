ad6a13ffa0ddb678fedafc5dec5896d5d01f58ea
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-07 14:22:17 +0300

    Edit code according to PEP8

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def shorten_link(token, url):
     headers = {
         "Authorization": token
@@ -13,7 +14,11 @@ def shorten_link(token, url):
     long_url = {
         "long_url": url
     }
-    response = requests.post("https://api-ssl.bitly.com/v4/bitlinks", headers=headers, json=long_url)
+    response = requests.post(
+        "https://api-ssl.bitly.com/v4/bitlinks",
+        headers=headers,
+        json=long_url,
+    )
     bitlink = response.json()["id"]
     return bitlink
 
@@ -22,8 +27,14 @@ def count_clicks(token, bitlink):
     headers = {
         "Authorization": token
     }
-    response = requests.get("https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary".format(bitlink), headers=headers)
+    response = requests.get(
+        "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary".format(
+            bitlink,
+        ),
+        headers=headers,
+    )
     return response.json()["total_clicks"]
+
 
 def main():
 
@@ -39,11 +50,12 @@ def main():
         bitlink = shorten_link(bitly_token, url_to_shorten)
         print("Битлинк {}.".format(bitlink))
         amount_of_clicks = count_clicks(bitly_token, bitlink)
-        print("По данному битлинку было соверешно следующее количество кликов: {}.".format(amount_of_clicks))
+        print("Было соверешно следующее количество кликов: {}.".format(
+            amount_of_clicks
+        ))
     except KeyError:
         print("Ссылка введена неверно.")
 
-    
-    
+
 if __name__ == "__main__":
     main()
