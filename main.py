2cbc3adcd0706425e5adbf0c950f4c43f7d5e438
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-07 13:44:03 +0300

    Edit function shorter_link

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def shorten_link(token, url):
         "long_url": url
     }
     response = requests.post("https://api-ssl.bitly.com/v4/bitlinks", headers=headers, json=long_url)
-    print("Битлинк", response.json()["id"])
+    bitlink = response.json()["id"]
+    return bitlink
 
 
 def main():
@@ -27,7 +28,7 @@ def main():
 
     bitly_token = os.getenv("BITLY_TOKEN")
 
-    shorten_link(bitly_token, url_to_shorten.url)
+    print("Битлинк", shorten_link(bitly_token, url_to_shorten.url))
 
     
 if __name__ == "__main__":
